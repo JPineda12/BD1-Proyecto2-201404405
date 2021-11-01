f46d3ecc68c5cc68f5025f08d2d6bef6b58cfa8b
Juan Pineda <jpinedaespino@gmail.com>
2021-10-31 23:48:23 -0600

    .

--- CargaDatos.sql
+++ CargaDatos.sql
@@ -24,14 +24,75 @@ SELECT * FROM Temporal;
 INSERT INTO Pais(pais)
 SELECT DISTINCT T.Pais FROM Temporal t;
 
-INSERT INTO Region(region, Pais_idPais)
-SELECT DISTINCT T.Region, P.idPais FROM Temporal T, Pais P 
-WHERE P.Pais = T.Pais;
+INSERT INTO Region(region)
+SELECT DISTINCT T.Region FROM Temporal T;
 
-INSERT INTO Departamento(departamento, Region_idRegion)
-SELECT DISTINCT T.Depto, R.idRegion FROM Temporal T, Region R
-WHERE T.Region = R.Region;
+INSERT INTO Pais_Region(Pais_idPais, Region_idRegion)
+SELECT DISTINCT P.idPais, R.idRegion FROM Temporal T, Pais P, Region R 
+WHERE P.pais = T.pais
+AND T.Region = R.region;
+
+
+
+INSERT INTO Departamento(departamento, PaisRegion_idPaisRegion)
+SELECT DISTINCT T.Depto, PR.idPaisRegion FROM Temporal T, Region R, Pais P, Pais_Region PR
+WHERE T.Region = R.Region
+AND P.pais = T.Pais
+AND PR.Pais_idPais = P.idPais
+AND PR.Region_idRegion = R.idRegion;
+
 
 INSERT INTO Municipio(municipio, Departamento_idDepartamento)
 SELECT DISTINCT T.Municipio, D.idDepartamento FROM Temporal T, Departamento D
-WHERE T.Municipio = D.departamento;
+WHERE T.depto = D.departamento;
+
+INSERT INTO Raza(raza)
+SELECT DISTINCT T.Raza FROM Temporal T;
+
+INSERT INTO Sexo(sexo)
+SELECT DISTINCT T.sexo FROM TEMPORAL T;
+
+INSERT INTO Escolaridad(escolaridad)
+VALUES('PRIMARIA');
+
+INSERT INTO Escolaridad(escolaridad)
+VALUES('NIVEL MEDIO');
+
+INSERT INTO Escolaridad(escolaridad)
+VALUES('UNIVERSITARIOS');
+
+INSERT INTO Educacion(educacion)
+VALUES('ANALFABETOS');
+
+INSERT INTO Educacion(educacion)
+VALUES('ALFABETOS');
+
+INSERT INTO Partido_Politico(partido, acronimo)
+SELECT DISTINCT T.nombre_partido, T.partido FROM TEMPORAL T;
+
+INSERT INTO Eleccion(eleccion, Municipio_idMunicipio, Partido_idPartido)
+SELECT DISTINCT T.Nombre_Eleccion, M.idMunicipio, P.idPartido
+FROM Temporal T, Municipio M, Partido_politico P
+WHERE T.Municipio = M.municipio
+AND P.partido = T.nombre_partido;
+
+INSERT INTO Educacion_Escolaridad(cantidad, Escolaridad_idEscolaridad, Educacion_idEducacion)
+SELECT DISTINCT T.Analfabetos, NULL, 1
+FROM Temporal T;
+
+INSERT INTO Educacion_Escolaridad(cantidad, Escolaridad_idEscolaridad, Educacion_idEducacion)
+SELECT DISTINCT T.Primaria, 1, 2
+FROM Temporal T;
+
+INSERT INTO Educacion_Escolaridad(cantidad, Escolaridad_idEscolaridad, Educacion_idEducacion)
+SELECT DISTINCT T.Nivel_Medio, 2, 2
+FROM Temporal T;
+
+INSERT INTO Educacion_Escolaridad(cantidad, Escolaridad_idEscolaridad, Educacion_idEducacion)
+SELECT DISTINCT T.Universitarios, 3, 2
+FROM Temporal T;
+
+INSERT INTO Votos(cantidad, Eleccion_idEleccion, Educacion_idEducacion, Raza_idRaza, Sexo_idSexo, Escolaridad_idEscolaridad)
+SELECT DISTINCT T.
+
+
--- ScriptCreacion.sql
+++ ScriptCreacion.sql
@@ -7,18 +7,27 @@ idPais INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 pais VARCHAR(100)
 );
 
+
+
 CREATE TABLE Region(
 idRegion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-region VARCHAR(100),
-Pais_idPais INT,
-FOREIGN KEY(Pais_idPais) REFERENCES Pais(idPais)
+region VARCHAR(100)
 );
+
+CREATE TABLE Pais_Region(
+idPaisRegion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+Pais_idPais INT,
+Region_idRegion INT,
+FOREIGN KEY(Pais_idPais) REFERENCES Pais(idPais),
+FOREIGN KEY(Region_idRegion) REFERENCES Region(idRegion)
+);
+
 
 CREATE TABLE Departamento(
 idDepartamento INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 departamento VARCHAR(100),
-Region_idRegion INT,
-FOREIGN KEY(Region_idRegion) REFERENCES Region(idRegion)
+PaisRegion_idPaisRegion INT,
+FOREIGN KEY(PaisRegion_idPaisRegion) REFERENCES Pais_Region(idPaisRegion)
 );
 
 CREATE TABLE Municipio(
@@ -48,44 +57,53 @@ idEducacion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 educacion VARCHAR(20)
 );
 
-CREATE TABLE Eleccion(
-idEleccion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-eleccion VARCHAR(45),
-Municipio_idMunicipio INT,
-FOREIGN KEY(Municipio_idMunicipio) REFERENCES Municipio(idMunicipio)
+CREATE TABLE Educacion_Escolaridad(
+idEducacionEscolaridad INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+cantidad INT,
+Escolaridad_idEscolaridad INT,
+Educacion_idEducacion INT,
+FOREIGN KEY(Escolaridad_idEscolaridad) REFERENCES Escolaridad(idEscolaridad),
+FOREIGN KEY(Educacion_idEducacion) REFERENCES Educacion(idEducacion)
 );
 
 CREATE TABLE Partido_Politico(
 idPartido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-partido VARCHAR(100)
+partido VARCHAR(100),
+acronimo VARCHAR(10)
 );
-
-CREATE TABLE Puesto(
-idPuesto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-puesto VARCHAR(50),
+ 
+CREATE TABLE Eleccion(
+idEleccion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+eleccion VARCHAR(45),
+Municipio_idMunicipio INT,
 Partido_idPartido INT,
+FOREIGN KEY(Municipio_idMunicipio) REFERENCES Municipio(idMunicipio),
 FOREIGN KEY(Partido_idPartido) REFERENCES Partido_Politico(idPartido)
 );
-
-CREATE TABLE Detalle_Eleccion(
-idDetalle_Eleccion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-Puesto_idPuesto INT,
-Eleccion_idEleccion INT,
-FOREIGN KEY(Puesto_idPuesto) REFERENCES Puesto(idPuesto),
-FOREIGN KEY(Eleccion_idEleccion) REFERENCES Eleccion(idEleccion)
-);
-
 CREATE TABLE Votos(
 idVotos INT,
 cantidad INT,
-Detalle_idEleccion INT,
-Educacion_idEducacion INT,
+Eleccion_idEleccion INT,
 Raza_idRaza INT,
 Sexo_idSexo INT,
-Escolaridad_idEscolaridad INT,
-FOREIGN KEY(Detalle_idEleccion) REFERENCES Detalle_Eleccion(idDetalle_Eleccion),
-FOREIGN KEY(Educacion_idEducacion) REFERENCES Educacion(idEducacion),
+EducacionEscolaridad_idEducacionEscolaridad INT,
+FOREIGN KEY(Eleccion_idEleccion) REFERENCES Eleccion(idEleccion),
 FOREIGN KEY(Raza_idRaza) REFERENCES Raza(idRaza),
-FOREIGN KEY(Escolaridad_idEscolaridad) REFERENCES Escolaridad(idEscolaridad)
-
+FOREIGN KEY(EducacionEscolaridad_idEducacionEscolaridad) REFERENCES Educacion_Escolaridad(idEducacionEscolaridad)
 );
+/*
+DROP TABLE Votos;
+DROP TABLE Educacion_Escolaridad;
+DROP TABLE Educacion;
+DROP TABLE Escolaridad;
+DROP TABLE Sexo;
+DROP TABLE Raza; 
+DROP TABLE Eleccion;
+DROP TABLE Partido_Politico;
+DROP TABLE Municipio;
+DROP TABLE Departamento;
+DROP TABLE Pais_Region;
+DROP TABLE Region;
+
+DROP TABLE Pais;
+*/
